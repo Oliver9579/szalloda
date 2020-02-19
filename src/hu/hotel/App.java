ba2c8eb6ce55c6fb474970906365b50e4d14e6ce
pfazekas <v-pfazekas@hotels.com>
2020-02-19 14:42:56 +0100

    Task 2, 3, 4

--- src/hu/hotel/App.java
+++ src/hu/hotel/App.java
@@ -4,14 +4,17 @@ import hu.hotel.controller.HotelService;
 import hu.hotel.model.service.DataApi;
 import hu.hotel.model.service.DataParser;
 import hu.hotel.model.service.FileReader;
+import hu.hotel.model.service.FileWriter;
 
 public class App {
 
     private final HotelService hotelService;
+    private final FileWriter fileWriter;
 
     private App() {
         DataApi dataApi = new DataApi(new FileReader(),  new DataParser());
         hotelService = new HotelService(dataApi.getBookings("pitypang.txt"));
+        fileWriter = new FileWriter("bevetel.txt");
 
     }
 
@@ -21,6 +24,11 @@ public class App {
 
     private void run() {
         System.out.println("2. feladat");
-        System.out.println(hotelService.getBooksCount());
+        System.out.println(hotelService.getLongestDetails());
+        fileWriter.write(hotelService.getPrices());
+        System.out.println("3.feladat");
+        System.out.println("A szálloda éves bevétele: " + hotelService.getTotalPrice());
+        System.out.println("4. feladat: ");
+        System.out.println(hotelService.getTotalGuestNightsDetails());
     }
 }
--- src/hu/hotel/controller/HotelService.java
+++ src/hu/hotel/controller/HotelService.java
@@ -2,17 +2,60 @@ package hu.hotel.controller;
 
 import hu.hotel.model.domain.Book;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class HotelService {
 
-    private List<Book> booking;
+    private List<Book> bookings;
 
-    public HotelService(List<Book> booking) {
-        this.booking = booking;
+    public HotelService(List<Book> bookings) {
+        this.bookings = bookings;
     }
 
-    public int getBooksCount() {
-        return booking.size();
+    public String getLongestDetails() {
+        Book longestStay = getLongestStay();
+        return String.format("%s (%d) - %d", longestStay.getName(), longestStay.getArrival(), longestStay.getDuration());
+    }
+
+    private Book getLongestStay() {
+        return bookings.stream()
+                .max(Comparator.comparing(Book::getDuration))
+                .get();
+    }
+
+    public List<String> getPrices() {
+        return bookings.stream()
+                .map(book -> book.getId() + ":" + book.getTotalPrice())
+                .collect(Collectors.toList());
+    }
+
+    public int getTotalPrice() {
+        return bookings.stream()
+                .mapToInt(Book::getTotalPrice)
+                .sum();
+
+    }
+
+    public String getTotalGuestNightsDetails() {
+        return getTotalGuestNights().entrySet().stream()
+                .map(i -> i.getKey() + ": " + i.getValue() + " vendégéj")
+                .collect(Collectors.joining("\r\n"));
+    }
+
+    private Map<Integer, Integer> getTotalGuestNights() {
+        Map<Integer, Integer> totalGuestNights = new TreeMap<>();
+        for(var book : bookings) {
+            Map<Integer, Integer> guestNightMap = book.getGuestNightMap();
+            for (var guestNight : guestNightMap.entrySet()) {
+                int base = totalGuestNights.containsKey(guestNight.getKey()) ? totalGuestNights.get(guestNight.getKey()) : 0;
+                int value = base + guestNight.getValue();
+                totalGuestNights.put(guestNight.getKey(), value);
+            }
+        }
+        return totalGuestNights;
     }
 }
